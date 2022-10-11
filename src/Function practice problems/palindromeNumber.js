c578e9c04a2df677eb8a640261fa3908215c6bed
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-10-11 15:54:42 +0530

    js basics

---
+++ src/Function practice problems/palindromeNumber.js
@@ -0,0 +1,22 @@
+function checkPalindrome(inputNum){
+    let i=inputNum;
+    let rev=0;
+    while (i > 0){
+        let remainder=i%10;
+        rev=parseInt(rev*10+remainder);
+        i=parseInt(i/10);
+    }
+    if (inputNum == rev) {
+        return true;
+    }else
+        return false;
+}
+const prompt = require("prompt-sync")();
+let number =parseInt( prompt("enter number to check palindrome: ") );
+let isPalindrome = checkPalindrome(number);
+
+if(isPalindrome){
+  console.log(number," is palindrome number");
+} else {
+    console.log(number," is not palindrome number");
+}
