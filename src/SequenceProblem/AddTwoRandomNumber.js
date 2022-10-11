0c0d81387376649b30e2ad68b159fa8625ad300b
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-10-11 16:06:27 +0530

    js basics

---
+++ src/SequenceProblem/AddTwoRandomNumber.js
@@ -0,0 +1,5 @@
+console.log("Addition of two random dice number is:");
+let diceNumber1 = Math.floor((Math.random() * 10));
+let diceNumber2 = Math.floor((Math.random() * 10));
+let addition = diceNumber1 + diceNumber2;
+console.log(addition);
