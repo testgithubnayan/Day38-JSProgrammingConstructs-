10d8c9ca1e10b2379925809965ee48139015490b
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-10-11 15:48:04 +0530

    js basics

---
+++ src/SelectionProblem_with-case/readNo_And_Display_Case.js
@@ -0,0 +1,18 @@
+var number = 10;
+ switch (number) {
+    case 1:
+        console.log("Unit")
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+    case 100:
+        console.log("Hundred");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    default:
+        console.log("Enter valid number")
+        break;
+}
