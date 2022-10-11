57d1f3266468c45780ad7177294fe8cbc40d6a96
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-10-11 16:08:51 +0530

    js basics

---
+++ src/SequenceProblem/sumAndAvg.js
@@ -0,0 +1,7 @@
+let sum = 0;
+for(let i = 0; i < 5; i++);
+let number = Math.floor((Math.random() * 90) + 10);
+sum += number;
+let average = sum / 5;
+console.log("The sum of 5 random 2 digit value is: " ,sum);
+console.log("The average of 5 random 2 digit value is: " ,average);
